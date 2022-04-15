a115ff3fc314ba3921545e597bec9ad1703f7efa
Anna <anya.shalashova@mail.ru>
2022-04-15 15:56:21 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+const name = "Ivan";
+alert(`Hello, ${name}`);
+confirm("Are you realy want to go out?");
+
+const obj = prompt('What is your name?');
+console.log(obj);
+console.log(typeof(obj));
